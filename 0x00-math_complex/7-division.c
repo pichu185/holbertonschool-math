07a7d39fafd37133825d7ced372430be8321dd68
Pichu185 <pichu_otegui@hotmail.com>
2021-09-02 01:13:40 +0000

    7-division.c

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * division - function that performs the division
+ * operation to complex numbers
+ * @c1: complex number
+ * @c2: complex number
+ * @c3: complex number
+ */
+
+void division(complex c1, complex c2, complex *c3)
+{
+
+}
